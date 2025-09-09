40c54b7efefbf4c41fee4de9247d81320aa38a51
Felix Linghu <felix.linghu@oocl.com>
2025-09-09 16:50:45 +0800

    feat:add pagination support and improve code formatting in EmployeeController and EmployeesTest

--- src/main/java/com/example/day7springboot/controller/EmployeeController.java
+++ src/main/java/com/example/day7springboot/controller/EmployeeController.java
@@ -2,7 +2,6 @@ package com.example.day7springboot.controller;
 
 
 import java.util.Objects;
-import org.springframework.context.annotation.Scope;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,11 +22,13 @@ import java.util.List;
 public class EmployeeController {
 
   private List<Employee> employees = new ArrayList<>();
+
   @GetMapping("")
   @ResponseStatus(HttpStatus.OK)
-  public List<Employee> getAllEmployees(){
+  public List<Employee> getAllEmployees() {
     return employees;
   }
+
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
   public Employee create(@RequestBody Employee employee) {
@@ -35,29 +36,43 @@ public class EmployeeController {
     employees.add(newEmployee);
     return newEmployee;
   }
+
   @GetMapping("/{id}")
   @ResponseStatus(HttpStatus.OK)
   public Employee getEmployeeById(@PathVariable Integer id) {
     return employees.stream().filter(employee -> Objects.equals(employee.id(), id)).findFirst().orElse(null);
   }
+
   @GetMapping(params = "gender")
   @ResponseStatus(HttpStatus.OK)
-  public List<Employee> getListByGender(@RequestParam(required = true) String gender){
+  public List<Employee> getListByGender(@RequestParam(required = true) String gender) {
     return employees.stream().filter(employee -> employee.gender().equals(gender)).toList();
   }
+
   @PutMapping("/{id}")
   @ResponseStatus(HttpStatus.OK)
-  public Employee updateEmployeeById(@PathVariable Integer id,@RequestBody Employee employee) {
+  public Employee updateEmployeeById(@PathVariable Integer id, @RequestBody Employee employee) {
     Employee employee2 = employees.stream().filter(employee1 -> Objects.equals(employee1.id(), id)).findFirst().orElse(null);
     employees.remove(employee2);
     employees.add(employee);
     return getEmployeeById(id);
   }
-  @DeleteMapping ("/{id}")
+
+  @DeleteMapping("/{id}")
   @ResponseStatus(HttpStatus.OK)
   public void updateEmployeeById(@PathVariable Integer id) {
     Employee employee2 = employees.stream().filter(employee1 -> Objects.equals(employee1.id(), id)).findFirst().orElse(null);
     employees.remove(employee2);
+  }
+
+  @GetMapping(params = {"page", "size"})
+  public List<Employee> pageQuery(@RequestParam int page, @RequestParam int size) {
+    int fromIndex = (page - 1) * size;
+    int toIndex = Math.min(fromIndex + size, employees.size());
+    if (fromIndex >= employees.size()) {
+      return new ArrayList<>();
+    }
+    return employees.subList(fromIndex, toIndex);
   }
 
   public void clear() {
--- src/test/java/com/example/day7springboot/EmployeesTest.java
+++ src/test/java/com/example/day7springboot/EmployeesTest.java
@@ -26,10 +26,12 @@ public class EmployeesTest {
   private MockMvc mockMvc;
   @Autowired
   private EmployeeController employeeController;
-@BeforeEach
-public void setUp(){
-  employeeController.clear();
-}
+
+  @BeforeEach
+  public void setUp() {
+    employeeController.clear();
+  }
+
   @Test
   public void should_return_created_employees_when_post() throws Exception {
     //given
@@ -50,12 +52,13 @@ public void setUp(){
         .andExpect(jsonPath("$.gender").value("Male"))
         .andExpect(jsonPath("$.salary").value(5000.0));
   }
+
   @Test
   public void should_return_employees_when_get_employees_id_correct() throws Exception {
     //given
     Employee employee = employeeController.create(new Employee(2, "John Smith", 32, "Male", 5000.0));
-    String id = "/"+employee.id();
-    MockHttpServletRequestBuilder request = get("/employees"+id).contentType(MediaType.APPLICATION_JSON);
+    String id = "/" + employee.id();
+    MockHttpServletRequestBuilder request = get("/employees" + id).contentType(MediaType.APPLICATION_JSON);
     mockMvc.perform(request)
         .andExpect(status().isOk())
         .andExpect(jsonPath("$.name").value("John Smith"))
@@ -63,6 +66,7 @@ public void setUp(){
         .andExpect(jsonPath("$.gender").value("Male"))
         .andExpect(jsonPath("$.salary").value(5000.0));
   }
+
   @Test
   public void should_return_male_employees_when_get_males() throws Exception {
     //given
@@ -77,6 +81,7 @@ public void setUp(){
         .andExpect(jsonPath("$[0].gender").value("Male"))
         .andExpect(jsonPath("$[0].salary").value(5000.0));
   }
+
   @Test
   public void should_return_all_employees_when_get_employees() throws Exception {
     //given
@@ -91,12 +96,13 @@ public void setUp(){
         .andExpect(jsonPath("$[0].gender").value("Male"))
         .andExpect(jsonPath("$[0].salary").value(5000.0));
   }
+
   @Test
   public void should_return_update_employees_when_update_employees() throws Exception {
     //given
     Employee employee1 = employeeController.create(new Employee(3, "John Smith", 32, "Male", 5000.0));
     Employee employee2 = employeeController.create(new Employee(4, "Vega Feng", 21, "FeMale", 5000.0));
-    String id="/"+employee1.id();
+    String id = "/" + employee1.id();
     String requestBody = """
                 {
                 "id":1,
@@ -106,29 +112,38 @@ public void setUp(){
                 "salary": 50000.0
                 }
         """;
-    MockHttpServletRequestBuilder request = put("/employees"+id).contentType(MediaType.APPLICATION_JSON).content(requestBody);
+    MockHttpServletRequestBuilder request = put("/employees" + id).contentType(MediaType.APPLICATION_JSON).content(requestBody);
     mockMvc.perform(request)
         .andExpect(status().isOk())
         .andExpect(jsonPath("$.age").value(320))
         .andExpect(jsonPath("$.salary").value(50000.0));
   }
+
   @Test
   public void should_return_null_when_delete_employees() throws Exception {
     //given
     Employee employee1 = employeeController.create(new Employee(3, "John Smith", 32, "Male", 5000.0));
     Employee employee2 = employeeController.create(new Employee(4, "Vega Feng", 21, "FeMale", 5000.0));
-    String id="/"+employee1.id();
+    String id = "/" + employee1.id();
 
-    MockHttpServletRequestBuilder request = delete("/employees"+id).contentType(MediaType.APPLICATION_JSON);
+    MockHttpServletRequestBuilder request = delete("/employees" + id).contentType(MediaType.APPLICATION_JSON);
     mockMvc.perform(request)
         .andExpect(status().isOk());
     assertNull(employeeController.getEmployeeById(employee1.id()));
 
   }
 
-
-
-
+  @Test
+  public void should_return_page_when_get_employees_with_page() throws Exception {
+    //given
+    Employee employee1 = employeeController.create(new Employee(3, "John Smith", 32, "Male", 5000.0));
+    Employee employee2 = employeeController.create(new Employee(4, "Vega Feng", 21, "FeMale", 5000.0));
+    Employee employee3 = employeeController.create(new Employee(4, "Vega Feng", 21, "FeMale", 5000.0));
+    MockHttpServletRequestBuilder request = get("/employees?page=1&size=2").contentType(MediaType.APPLICATION_JSON);
+    mockMvc.perform(request)
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.length()").value(2));
+  }
 
 
 }
